7a9e9c295869180b8caf0d47ca0affdc9226ac2b
Fredwlassow <Fredwlassow13@gmail.com>
2026-04-25 17:26:13 -0300

    feat: atualizações gerais do projeto

--- src/perfil_psicologo/dto/update-perfil_psicologo.dto.ts
+++ src/perfil_psicologo/dto/update-perfil_psicologo.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePerfilPsicologoDto } from './create-perfil_psicologo.dto';
 
-export class UpdatePerfilPsicologoDto extends PartialType(CreatePerfilPsicologoDto) {}
+export class UpdatePerfilPsicologoDto extends PartialType(
+  CreatePerfilPsicologoDto,
+) {}
