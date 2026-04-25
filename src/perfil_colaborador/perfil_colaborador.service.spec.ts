41c4649c9977f6130f8ae5f8da563a311b37d7ce
Fredwlassow <Fredwlassow13@gmail.com>
2026-04-25 17:25:46 -0300

    fix: adiciona mocks do PrismaService nos spec de perfil

--- src/perfil_colaborador/perfil_colaborador.service.spec.ts
+++ src/perfil_colaborador/perfil_colaborador.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PerfilColaboradorService } from './perfil_colaborador.service';
+import { PrismaService } from 'src/database/prisma.service';
+
+const prismaMock = {
+  perfilColaborador: {
+    create: jest.fn(),
+    findMany: jest.fn(),
+    findUnique: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  },
+  usuario: {
+    findUnique: jest.fn(),
+  },
+};
 
 describe('PerfilColaboradorService', () => {
   let service: PerfilColaboradorService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PerfilColaboradorService],
+      providers: [
+        PerfilColaboradorService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+      ],
     }).compile();
 
     service = module.get<PerfilColaboradorService>(PerfilColaboradorService);
