4020ce864936af6cef86d4a80755889d0e3e0dd1
Zeca <zeca1609@gmail.com>
2026-04-25 17:29:37 -0300

    feat: update Prisma configuration and add PostgreSQL support

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,4 +1,6 @@
+import 'dotenv/config';
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
@@ -6,6 +8,18 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    super({
+      adapter: new PrismaPg({ connectionString }),
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
